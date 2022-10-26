16c433d8383118ffc2e602648e534b761d99dfd0
dmsvk0217 <116181378+dmsvk0217@users.noreply.github.com>
2022-10-26 20:35:23 +0900

    request_handler / post_handler 구현

--- my_server.c
+++ my_server.c
@@ -5,17 +5,29 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
-#define PORT_NUMBER 7779
+#include <netdb.h>
+#include <signal.h>
+#include <fcntl.h>
+#define PORT_NUMBER 7776
 #define BACKLOG 10
+#define BUFSIZE 65536
+#define SEND_MESSAGE_BUFSIZE 1024
+char* CURR_MY_PATH_ROOT;
 
-int main(){
+void error_handling(char *);
+void GET_handler(char *, char *, char *, int);
+void request_handler(void *);
+
+int main(int argc, char* argv[]){
   int server_socket;
   int client_socket;
   struct sockaddr_in server_address;
   struct sockaddr_in client_address;
-  char message[800];
-  int str_len;
+  char message[BUFSIZE];
+  int client_address_size;
   int nRcv;
+
+  CURR_MY_PATH_ROOT = getenv("PWD");
 
   if((server_socket = socket(PF_INET, SOCK_STREAM, 0))==-1){
     perror("socket");
@@ -37,18 +49,99 @@ int main(){
   }
 
   while(1){
-    str_len = sizeof(struct sockaddr_in);
-    if((client_socket = accept(server_socket, (struct sockaddr*)&client_address, (socklen_t *)&str_len)) == -1){
+    client_address_size = sizeof(struct sockaddr_in);
+    if((client_socket = accept(server_socket, (struct sockaddr*)&client_address, (socklen_t *)&client_address_size)) == -1){
       perror("accept");
       continue;
     }
-    nRcv = recv(client_socket, message, sizeof(message) - 1, 0);
-    printf("Receive Message : %s", message);
 
-    printf("server : got connection form %s\n", inet_ntoa(client_address.sin_addr));
+    printf("Connection Request : %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+
+    request_handler(&client_socket);
   }
+  close(client_socket);
+  return 0;
+}
 
 
-  
+void request_handler(void *arg){
+  char msg[BUFSIZE];
+	char *firstLine[3];
 
+	int sd = *(int*)arg;
+
+	int rcvd = recv(sd, msg, BUFSIZE-1, 0);
+	if(rcvd<=0) error_handling("Error about recv()!!");
+	printf("-----------Request message from Client-----------\n");
+	printf("%s",msg);
+	printf("\n-------------------------------------------------\n");
+
+	char post_information[SEND_MESSAGE_BUFSIZE];
+	char *curr_msg = NULL;
+	char METHOD[4]="";
+	char VERSION[10]="";
+	char URL[SEND_MESSAGE_BUFSIZE]="";
+
+	curr_msg = strtok(msg, "\n");
+	
+	int line_count=1;
+	while(curr_msg){
+		if(line_count>=15) strcpy(post_information, curr_msg);
+    printf("[%d]%s\n", line_count, curr_msg);
+		curr_msg = strtok(NULL, "\n");
+		line_count++;
+	}
+
+	firstLine[0] = strtok(msg, " ");
+	firstLine[1] = strtok(NULL, " ");
+	firstLine[2] = strtok(NULL, " ");
+	
+	strcpy(METHOD, firstLine[0]);
+	strcpy(URL, firstLine[1]);
+	strcpy(VERSION, firstLine[2]);
+	
+  //strncmp 같으면 0 return
+	if(!strncmp(METHOD, "GET",3)) GET_handler(VERSION, msg, URL, sd);
+
+	shutdown(sd, SHUT_RDWR);
+	close(sd);
+}
+
+void GET_handler(char *V, char *message, char *U, int client){
+	int fd, str_len;
+	char SEND_DATA[SEND_MESSAGE_BUFSIZE];
+	char FIANL_PATH[BUFSIZE];
+	char VERSION[10]="";
+	char URL[SEND_MESSAGE_BUFSIZE]="";
+	
+	strcpy(VERSION, V);
+	strcpy(URL, U);
+
+	if(strncmp(VERSION, "HTTP/1.0",8)!=0 && strncmp(VERSION, "HTTP/1.1",8)!=0){
+		write(client, "HTTP/1.1 400 Bad Request\n",25);
+	}
+	
+	if( strlen(URL) == 1 && !strncmp(URL, "/",1)) strcpy(URL, "/index.html");
+
+	strcpy(FIANL_PATH, CURR_MY_PATH_ROOT);
+	strcat(FIANL_PATH, URL);
+
+	if((fd=open(FIANL_PATH, O_RDONLY)) != -1){
+		send(client, "HTTP/1.0 200 OK\n\n", 17, 0);
+		while(1){
+			str_len = read(fd, SEND_DATA, SEND_MESSAGE_BUFSIZE);
+			if(str_len<=0) break;
+			write(client, SEND_DATA, str_len);
+		}
+	}
+	else {
+		write(client, "HTTP/1.1 404 Not Found\n", 23);
+	}
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
